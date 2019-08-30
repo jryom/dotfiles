c9fb3cf8074ce2539172fdd4e981af1a03acfa76
Jesper Ryom <jesper@ryom.me>
2019-08-30 12:01:40 +0200

    add some bitbar plugins

---
+++ bitbar/check-unread-emails.1m.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# <bitbar.title>Fastmail unread messages</bitbar.title>
+# <bitbar.version>v1.0</bitbar.version>
+# <bitbar.author>Jesper Ryom</bitbar.author>
+# <bitbar.author.github>jesperryom</bitbar.author.github>
+# <bitbar.desc>Plugin to display a simple count of unread messages in Fastmail (or any other IMAP server). Reads from .netrc</bitbar.desc>
+# <bitbar.dependencies>curl, bash</bitbar.dependencies>
+
+number=`curl -s -n imaps://imap.fastmail.com -X 'STATUS INBOX (UNSEEN)' | tr -d -c 0-9`
+if [ $number -gt 0 ]; then
+    echo -e "﫯 | font=Iosevka href=https://www.fastmail.com/mail/"
+fi
