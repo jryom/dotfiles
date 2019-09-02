994860500131e6cbdd1caa2c7282297f9b61b48f
Jesper Ryom <jesper@ryom.me>
2019-09-02 19:18:41 +0200

    open mail.app when clicking email icon

--- bitbar/check-unread-emails.1m.sh
+++ bitbar/check-unread-emails.1m.sh
@@ -7,7 +7,8 @@
 # <bitbar.desc>Plugin to display a simple count of unread messages in Fastmail (or any other IMAP server). Reads from .netrc</bitbar.desc>
 # <bitbar.dependencies>curl, bash</bitbar.dependencies>
 
+
 number=`curl -s -n imaps://imap.fastmail.com -X 'STATUS INBOX (UNSEEN)' | tr -d -c 0-9`
 if [ $number -gt 0 ]; then
-    echo -e "﫯 | font=Iosevka href=https://www.fastmail.com/mail/"
+    echo -e "﫯 | font=Iosevka bash=/bin/bash param1=-c param2='/usr/bin/open /applications/mail.app' terminal=false"
 fi
