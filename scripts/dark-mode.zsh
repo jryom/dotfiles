078a6d27411bd63d27b058efa32a3e9995c55f29
Jesper Ryom <jesper@ryom.me>
2020-09-11 08:30:18 +0200

    improve dark mode script

--- scripts/dark-mode.zsh
+++ scripts/dark-mode.zsh
@@ -2,7 +2,13 @@
 PATH="/usr/local/bin:$PATH"
 
 isDark=$(defaults read -g AppleInterfaceStyle &>/dev/null && echo 1 || echo 0)
-isDaylight=$(/usr/libexec/corebrightnessdiag nightshift-internal | grep isDaylight | grep -Eo '[[:digit:]]')
+riseUTC=$(/usr/libexec/corebrightnessdiag nightshift-internal | grep sunrise | grep -o '".*"' | sed 's/"//g')
+setUTC=$(/usr/libexec/corebrightnessdiag nightshift-internal| grep sunset | grep -o '".*"' | sed 's/"//g')
+riseLocal=$(gdate --date "$riseUTC"  +"%H%M")
+setLocal=$(gdate --date "$setUTC"  +"%H%M")
+now=$(gdate +"%H%M")
+
+(( now > riseLocal && now < setLocal )) && isDaylight=1 || isDaylight=0
 
 if (( isDaylight == 0 && isDark == 0 )); then
    osascript -e 'tell app "System Events" to tell appearance preferences to set dark mode to true'
