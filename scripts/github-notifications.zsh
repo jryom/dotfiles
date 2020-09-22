3bb4079613ca5a4ef31f8dab9aed2bc262765370
Jesper Ryom <jesper@ryom.me>
2020-09-22 21:04:58 +0200

    add some new scripts - github desktop notifications - purge completed reminders

---
+++ scripts/github-notifications.zsh
@@ -0,0 +1,10 @@
+#!/bin/zsh
+PATH="/usr/local/bin:$PATH"
+
+USER=$(git config user.email)
+TOKEN=$(security find-generic-password -a github_token -w)
+
+curl -s -u $USER:$TOKEN  https://api.github.com/notifications \
+   | jq -r '.[] | select(.unread) | [.subject.type, .reason, .subject.title] | @sh' \
+   | xargs -n 3 sh -c \
+   'terminal-notifier -group "Github" -title "GitHub Notification" -subtitle "$0 $1" -message "$2" -open "https://github.com/notifications"'
