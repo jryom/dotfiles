6f710b850a2f4fc20f5f042bac0d8c6b70111b1e
Jesper Ryom <jesper@ryom.me>
2020-09-28 13:21:06 +0200

    wm tweaks

--- yabai/yabai-destroy_empty_spaces.sh
+++ yabai/yabai-destroy_empty_spaces.sh
@@ -1,4 +1,11 @@
-yabai -m query --spaces --display | \
-     jq -re 'map(select(."native-fullscreen" == 0)) | length > 1' \
-     && bash <(yabai -m query --spaces | \
-          jq -re 'map(select(."windows" == [] and ."focused" == 0)) | .[]| "yabai -m space \(.index|@sh) --destroy"')
+#!/bin/zsh
+
+spaces=$(yabai -m query --spaces --display)
+multiplespaces=$(echo "$spaces" | jq -re 'map(select(."native-fullscreen" == 0)) | length > 1')
+
+if [[ "$multiplespaces" = "true" ]]; then
+  destroyable=$(echo "$spaces" | jq -re 'map(select(."windows" == [] and ."visible" == 0)) | .[] | .index ')
+  if [[ "$destroyable" != "" ]]; then
+    yabai -m space "$destroyable" --destroy
+  fi
+fi
