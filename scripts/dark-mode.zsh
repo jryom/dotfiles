231c8445704d1a11f476f51cbbcd4ae3c8bb1b28
Jesper Ryom <jesper@ryom.me>
2020-10-11 11:23:54 +0200

    git tweaks

--- scripts/dark-mode.zsh
+++ scripts/dark-mode.zsh
@@ -3,3 +3,5 @@
 theme=$([[ "$argv" = "light" ]] && echo "$THEME_LIGHT" || echo "$THEME_DARK")
 kitty @ --to unix:/tmp/mykitty set-colors -a -c ~/.config/kitty/themes/$theme.conf
 ln -sf ~/.config/kitty/themes/$theme.conf ~/.config/kitty/theme.conf
+mkdir -p "$HOME/Library/Application Support/jesseduffield/lazygit"
+ln -sf $DOTDIR/lazygit/config-$argv.yml "$HOME/Library/Application Support/jesseduffield/lazygit/config.yml"
