e04ec706612171a2c0a5da4373eb157fca44239c
Jesper Ryom <2834949+iamjesper@users.noreply.github.com>
2020-10-30 09:58:45 +0100

    images in gh notifications

--- scripts/github-notifications.zsh
+++ scripts/github-notifications.zsh
@@ -5,6 +5,6 @@ USER=$(git config user.email)
 TOKEN=$(security find-generic-password -a github_token -w)
 
 curl -s -u $USER:$TOKEN  https://api.github.com/notifications \
-   | jq -r '.[] | select(.unread) | [.subject.type, .reason, .subject.title] | @sh' \
-   | xargs -n 3 sh -c \
-   'terminal-notifier -group "Github" -title "GitHub Notification" -subtitle "$0 $1" -message "$2" -open "https://github.com/notifications"'
+   | jq -r '.[] | select(.unread) | [.subject.type, .reason, .subject.title, .repository.owner.avatar_url] | @sh' \
+   | xargs -n 4 sh -c \
+   'terminal-notifier -group "Github" -title "GitHub Notification" -subtitle "$0 $1" -message "$2" -open "https://github.com/notifications" -appIcon "https://github.com/fluidicon.png" -contentImage "$3"'
