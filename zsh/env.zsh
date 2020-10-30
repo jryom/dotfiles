4acbd2a257c32d20a9024414f21a78e11f633ff1
Jesper Ryom <2834949+iamjesper@users.noreply.github.com>
2020-10-30 09:59:02 +0100

    update env

--- zsh/env.zsh
+++ zsh/env.zsh
@@ -6,14 +6,20 @@ export FZF_CTRL_T_OPTS="--delimiter '/' --nth '-1' --preview '([[ -d {} ]] && tr
 export FZF_THEME_DARK='--color=16,bg+:0'
 export FZF_THEME_LIGHT='--color=16,bg+:7'
 export FZF_DEFAULT_COMMAND="rg --files --hidden"
-export FZF_DEFAULT_OPTS="--ansi $([[ "$DARK_MODE_ACTIVE" = 1 ]] && echo "$FZF_THEME_DARK" || echo "$FZF_THEME_LIGHT") --no-height"
+export FZF_DEFAULT_OPTS="--ansi $([[ "$DARK_MODE_ACTIVE" = 1 ]] && echo "$FZF_THEME_DARK" || echo "$FZF_THEME_LIGHT")"
 export HISTFILE=~/Documents/.config/zsh-history
 export HISTFILESIZE=1000000
 export HISTSIZE=1000000
 export HISTTIMEFORMAT="[%F %T] "
 export KEYTIMEOUT=1
-export NODE_VERSION=14
+export NODE_VERSION=15
 export LC_ALL="en_US.UTF-8"
-export PATH=$HOME/Library/Python/3.8/bin:/usr/local/lib/python3.8/site-packages/pip:/usr/local/bin:$PATH
 export RIPGREP_CONFIG_PATH="$HOME/.ripgreprc"
 export VISUAL="$EDITOR"
+
+PATH=$PATH
+PATH+=$( gfind $HOME/Library/Python -maxdepth 2 -type d -printf ":%p" )
+PATH+=$( python3 -c "import sysconfig; print(sysconfig.get_path('purelib'))" )
+PATH+=/usr/local/bin
+
+export PATH
