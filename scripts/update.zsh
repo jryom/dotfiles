25526d18536ddaff034470eeacd0e36f2f8ff798
Jesper Ryom <2834949+jryom@users.noreply.github.com>
2021-05-11 10:44:19 +0200

    spaces

--- scripts/update.zsh
+++ scripts/update.zsh
@@ -13,45 +13,38 @@ terminal-notifier -title "Upgrade script" -message "Running..." -group "Upgrade 
 
 printf "Running brew update...\n"            | ts
 brew update
-printf "\n\n\n"
+printf "\n\n"
 
 printf "Running brew upgrade...\n"           | ts
 brew upgrade --fetch-HEAD --greedy
-printf "\n\n\n"
+printf "\n\n"
 
 printf "Running brew cleanup...\n"           | ts
 HOMEBREW_CLEANUP_MAX_AGE_DAYS=60
 brew cleanup
-printf "\n\n\n"
+printf "\n\n"
 
 printf "Updating Node installation...\n"     | ts
 fnm install --lts && fnm use lts-latest && fnm default lts-latest
-printf "\n\n\n"
+printf "\n\n"
 
 printf "Updating global NPM packages...\n" | ts
-ncu -gse 2 || npm update -g
-printf "\n\n\n"
-
-printf "Update submodules...\n" | ts
-current_dir=$PWD
-cd $DOTDIR
-git submodule update --recursive --remote
-cd $current_dir
-printf "\n\n\n"
+ncu -gse 2 || npm update -g
+printf "\n\n"
 
 printf "Updating TLDR local data...\n"       | ts
 tldr --update
-printf "\n\n\n"
+printf "\n\n"
 
 printf "Updating antibody plugins...\n"      | ts
 antibody bundle < "$DOTDIR/zsh/zsh_plugins" > ~/.zsh_plugins
-printf "\n\n\n"
+printf "\n\n"
 
 printf "Updating Python packages...\n"       | ts
 python3 -m pip install --user --upgrade $(cat "$DOTDIR/pip-packages" | tr '\n' ' ')
-printf "\n\n\n"
+printf "\n\n"
 
 terminal-notifier -title "Upgrade script" -message "Finished!" -group "Upgrade script"
-printf "\n\n\n"
+printf "\n\n"
 
 date +'%s' > "/tmp/lastupdatesys"
