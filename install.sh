378c7141943ac1a9fbd4ec59b9ec66703ef3ced8
Jesper Ryom <2834949+jryom@users.noreply.github.com>
2021-07-26 15:47:21 +0200

    use verbose mode in scripts

--- install.sh
+++ install.sh
@@ -1,22 +1,19 @@
 #!/bin/bash
 
 trap 'ret=$?; test $ret -ne 0 && printf "Failed!\n" >&2; exit $ret' EXIT
-set -e
+set -ev
 
 # Ask for the sudo permission upfront and keep valid until EOS
 sudo -v
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
-# Store script execution path
 script_path="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
 source "$script_path/zsh/env.zsh"
 
 # Disable Gatekeeper if active
 if spctl --status > /dev/null; then
-  echo "Gatekeeper enabled, enter password to disable..."
   sudo spctl --master-disable
-  echo "Gatekeeper disabled."
 fi
 
 # Change some macOS default settings and restart dock
@@ -37,52 +34,36 @@ defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool
 defaults write com.apple.AppleMultitouchTrackpad Clicking -bool true
 killall Dock
 
-echo -n "Checking command line tools installation..."
 if ! type xcode-select >&- && xpath=$( xcode-select --print-path ) &&
    test -d "${xpath}" && test -x "${xpath}"; then
-   echo " Not installed. Install and run script again."
+   echo "Command line tools not installed. Install and run script again."
    xcode-select --install
    EXIT
 fi
-echo " OK!"
 
 if ! command -v brew >/dev/null; then
-  echo "Installing Homebrew..."
   curl -fsS 'https://raw.githubusercontent.com/Homebrew/install/master/install' | ruby
   export PATH="/usr/local/bin:$PATH"
 fi
 
-echo -n "Looking for missing brew packages... "
 if ! brew bundle check --file="$script_path/Brewfile" >/dev/null; then
-  echo "Dependencies missing! Brewing..."
   brew bundle install --file="$script_path/Brewfile" --force --no-lock
   if [ ! -f /private/etc/sudoers.d/yabai  ]; then
-    echo "Adding yabai to sudoers..."
     sudo zsh -c "echo '$(whoami) ALL = (root) NOPASSWD: $(which yabai) --load-sa' >> /private/etc/sudoers.d/yabai"
   fi
 fi
-echo "Done!"
 
-echo -n "Installing fzf... "
 echo y | $(brew --prefix)/opt/fzf/install
-echo "Done!"
 
-echo -n "Installing pip packages... "
 python3 -m pip install --user --upgrade $(cat "$script_path/pip-packages" | tr '\n' ' ')
-echo "Done!"
 
-echo -n "Symlinking files... "
 sudo python3 -m dotbot -c "$script_path/install.conf.yaml"
-echo "Done!"
 
-echo -n "Installing node... "
 eval "$(fnm env)"
 fnm install --lts && fnm use lts-latest && fnm default lts-latest
 
-echo -n "Installing global NPM packages... "
 npm install --loglevel silent --no-progress -g \
   $(cat "$script_path/npm-global-packages" | tr '\n' ' ')
-echo "Done!"
 
 # Use custom zsh install rather than bundled version
 ! [[ "$( which zsh )" = "/usr/local/bin/zsh" ]] && sudo dscl . -create /Users/$USER UserShell /usr/local/bin/zsh
@@ -104,4 +85,3 @@ loginitems -a "SpaceId"
 brew services start koekeishiya/formulae/yabai
 brew services start koekeishiya/formulae/skhd
 
-echo "Installation finished!"
--- scripts/update.zsh
+++ scripts/update.zsh
@@ -7,50 +7,29 @@ SUDO_ASKPASS="$HOME/.askpass" \
 eval "$(fnm env)"
 
 trap 'ret=$?; test $ret -ne 0 && terminal-notifier -group "Upgrade script" -title "Upgrade script" -message "Encountered an error! Check the logs for details."; exit $ret' EXIT
-set -e
+set -ev
 
 terminal-notifier -title "Upgrade script" -message "Running..." -group "Upgrade script"
 
-printf "Running brew update...\n"            | ts
 brew update
-printf "\n\n"
-
-printf "Running brew upgrade...\n"           | ts
 brew upgrade --fetch-HEAD --greedy
-printf "\n\n"
-
-printf "Running brew cleanup...\n"           | ts
 HOMEBREW_CLEANUP_MAX_AGE_DAYS=60
 brew cleanup
-printf "\n\n"
 
-printf "Updating Node installation...\n"     | ts
 fnm install --lts && fnm use lts-latest && fnm default lts-latest
-printf "\n\n"
 
-printf "Looking for missing global NPM modules...\n" | ts
 installed="$(npm ls --global --json --depth 0 | jq '.dependencies | length')"
 required="$(wc -l < $DOTDIR/npm-global-packages | xargs)"
 if (( $installed < $required )); then npm install -g $(cat "$DOTDIR/npm-global-packages" | tr '\n' ' '); fi
-printf "\n\n"
 
-printf "Updating global NPM packages...\n" | ts
 ncu -gse 2 || npm update -g
-printf "\n\n"
 
-printf "Updating TLDR local data...\n"       | ts
 tldr --update
-printf "\n\n"
 
-printf "Updating antibody plugins...\n"      | ts
 antibody bundle < "$DOTDIR/zsh/zsh_plugins" > ~/.zsh_plugins
-printf "\n\n"
 
-printf "Updating Python packages...\n"       | ts
 python3 -m pip install --user --upgrade $(cat "$DOTDIR/pip-packages" | tr '\n' ' ')
-printf "\n\n"
 
 terminal-notifier -title "Upgrade script" -message "Finished!" -group "Upgrade script"
-printf "\n\n"
 
 date +'%s' > "/tmp/lastupdatesys"
