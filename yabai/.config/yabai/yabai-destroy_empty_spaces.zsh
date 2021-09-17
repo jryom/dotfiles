6f358f8608684bb76314ab6646403c7061bf7162
Jesper Ryom <2834949+jryom@users.noreply.github.com>
2021-09-17 21:15:31 +0200

    fix destroy empty spaces script

--- yabai/.config/yabai/yabai-destroy_empty_spaces.zsh
+++ yabai/.config/yabai/yabai-destroy_empty_spaces.zsh
@@ -1,9 +1,5 @@
-spaces=$(yabai -m query --spaces)
-multiplespaces=$(echo "$spaces" | jq -re 'map(select(."native-fullscreen" == 0)) | length > 1')
+#!/usr/bin/env zsh
 
-if [[ "$multiplespaces" = "true" ]]; then
-  destroyable=$(echo "$spaces" | jq -re 'map(select(."windows" == [] and ."visible" == 0)) | .[0] | .index ')
-  if [[ ! -z "$destroyable" ]]; then
-    yabai -m space "$destroyable" --destroy
-  fi
-fi
+yabai -m query --spaces | \
+  jq -re 'map(select(."native-fullscreen" == 0 and ."windows" == [] and ."visible" == 0) | .index) | reverse | join("\n")' | \
+  xargs -I % sh -c 'yabai -m space "%" --destroy'
