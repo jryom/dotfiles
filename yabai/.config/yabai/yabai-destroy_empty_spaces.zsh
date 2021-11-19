20ed2a863c09d537a71e27579e9defd47e6bcd0c
Jesper Ryom <2834949+jryom@users.noreply.github.com>
2021-11-19 08:42:43 +0100

    yabai breaking changes

--- yabai/.config/yabai/yabai-destroy_empty_spaces.zsh
+++ yabai/.config/yabai/yabai-destroy_empty_spaces.zsh
@@ -1,5 +1,5 @@
 #!/usr/bin/env zsh
 
 yabai -m query --spaces | \
-  jq -re 'map(select(."native-fullscreen" == 0 and ."windows" == [] and ."visible" == 0) | .index) | reverse | join("\n")' | \
+  jq -re 'map(select(."is-native-fullscreen" == false and ."windows" == [] and ."is-visible" == false) | .index) | reverse | join("\n")' | \
   xargs -I % sh -c 'yabai -m space "%" --destroy'
