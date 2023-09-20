b7601f80c641f6ded21837ca5e4dfc934045a177
Jesper Ryom <2834949+jryom@users.noreply.github.com>
2023-09-20 09:43:44 +0200

    no notes session

--- home/.config/nvim/lua/plugins/auto-session.lua
+++ home/.config/nvim/lua/plugins/auto-session.lua
@@ -1,4 +1,9 @@
 return {
   "rmagatti/auto-session",
-  opts = { log_level = "error" },
+  opts = {
+    auto_session_suppress_dirs = {
+      "~/Documents/Notes",
+    },
+    log_level = "error",
+  },
 }
