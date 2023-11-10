9b0a6388154e739f775cfab0bd969dada982969d
Jesper Ryom <2834949+jryom@users.noreply.github.com>
2023-11-10 09:13:16 +0100

    image previews in lf

--- home/config/lf/preview.sh
+++ home/config/lf/preview.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
 
-case $(file --mime-type "$1" -b) in
+file=$1
+w=$2
+h=$3
+x=$4
+y=$5
+
+case $(file -Lb --mime-type "$1") in
+image*) kitty +kitten icat --silent --stdin no --transfer-mode file --place "${w}x${h}@${x}x${y}" "$file" </dev/null >/dev/tty && exit 1 ;;
 text/* | */json) bat -pp --italic-text=always --color=always "$1" ;;
 inode/directory | application/x-directory) tree --dirsfirst -I ".DS_Store" -D -pha -C -L 1 "$1" ;;
 *) mediainfo "$1" ;;
