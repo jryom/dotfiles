6f45207217134179a3fa17be799aac9042bf7aaa
Jesper Ryom <2834949+jryom@users.noreply.github.com>
2024-11-01 15:54:34 +0100

    back to delta

--- home/config/fish/config.fish
+++ home/config/fish/config.fish
@@ -123,4 +123,13 @@ if status is-interactive
     end
 
     fzf_configure_bindings --directory=\ct --variables=\e\cv
+
+    function delta_sidebyside --on-signal WINCH
+        if test "$COLUMNS" -ge 120; and ! contains side-by-side "$DELTA_FEATURES"
+            set --global --export --append DELTA_FEATURES side-by-side
+        else if test "$COLUMNS" -lt 120; and contains side-by-side "$DELTA_FEATURES"
+            set --erase DELTA_FEATURES[(contains --index side-by-side "$DELTA_FEATURES")]
+        end
+    end
+    delta_sidebyside
 end
